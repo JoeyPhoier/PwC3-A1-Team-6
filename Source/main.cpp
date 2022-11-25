b9837d46fa12d1aac688307ff9bb03acd7dd9ef6
Joey Phoier <114338319+JoeyPhoier@users.noreply.github.com>
2022-11-25 17:50:25 +0100

    Manual Scrolling
    Added some controls to test the scrolling, and played a bit with movement. We could end up removing acceleration for movement, since its not really necessary; it just makes it look and feel nicer.

--- Source/main.cpp
+++ Source/main.cpp
@@ -38,8 +38,28 @@ public:
     int backgroundscroll=0;
 };
 
+class Entity {
+public:
+    int health = 0;
+    Vector2 pos = { 0,0 };
+    Vector2 speed = { 0,0 };
+    Vector2 accel = { 0,0 };
+    const Vector2 maxaccel = { 3,3 };
+    const Vector2 maxspeed = { 40,50 };
+
+    void movement(){
+        speed.x += accel.x;
+        if (speed.x > maxspeed.x)speed.x = maxspeed.x;
+        else if (speed.x < -maxspeed.x)speed.x = -maxspeed.x;
+        speed.y += accel.y;
+        pos.x += speed.x;
+        pos.y += speed.y;
+    }
+};
+
 Stage stage1;
 
+void PlayerInput(Entity* player);
 
 int main(void)
 {
@@ -56,14 +76,16 @@ int main(void)
     Texture2D groundtexture = LoadTexture("assets/ground.png");
     SetWindowIcon(test);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);
+
+    Entity Rob;
     //--------------------------------------------------------------------------------------
     
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
+    {   
+        int currDisplay = GetCurrentMonitor();
         if ( IsKeyPressed(KEY_F11) || (IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT)))) {
-            int currDisplay = GetCurrentMonitor();
             
             if (!IsWindowFullscreen()) {
                 SetWindowMonitor(currDisplay);
@@ -78,14 +100,25 @@ int main(void)
             }
             
         }
-        stage1.groundscroll-= 3;
+        stage1.groundscroll-= 10;
         if (stage1.groundscroll <= -groundtexture.width) stage1.groundscroll = 0;
+
+        PlayerInput(&Rob);
+
         
         BeginDrawing();
         
         ClearBackground(RAYWHITE);
-        DrawTexture(groundtexture, stage1.groundscroll, 0, WHITE);
-        DrawTexture(groundtexture, groundtexture.width + stage1.groundscroll, 0, WHITE);
+        
+
+        Rectangle source = { Rob.pos.x, 0, groundtexture.width, groundtexture.height };
+        Rectangle dest = { 0, 680 * (GetScreenHeight() / 1080.0f), (GetScreenWidth() / 1920.0f) * groundtexture.width, (GetScreenHeight() / 1080.0f) * groundtexture.height};
+
+        DrawTexturePro(groundtexture, source, dest, Vector2{0,0}, 0, WHITE);
+
+
+        //DrawTexture(groundtexture, stage1.groundscroll, 0, WHITE);
+        //DrawTexture(groundtexture, groundtexture.width + stage1.groundscroll, 0, WHITE);
         
         //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
@@ -99,4 +132,30 @@ int main(void)
     //--------------------------------------------------------------------------------------
 
     return 0;
+}
+
+void PlayerInput(Entity* player){
+    if (IsKeyDown(KEY_A) && !IsKeyDown(KEY_D)) {
+        player->accel.x = -player->maxaccel.x;
+        if (player->speed.x > player->maxspeed.x * .35) player->speed.x = -player->maxspeed.x * .35;
+    }
+    else if (IsKeyDown(KEY_D) && !IsKeyDown(KEY_A)) {
+        player->accel.x = player->maxaccel.x;
+        if (player->speed.x < -player->maxspeed.x * .35) player->speed.x = player->maxspeed.x * .35;
+    }
+    else {
+        if (player->speed.x > 0)
+        {
+            if (player->maxaccel.x/2 > player->speed.x)player->accel.x = -player->speed.x;
+            else player->accel.x = -player->maxaccel.x / 2;
+
+        }
+        else if (player->speed.x < 0)
+        {
+            if (player->maxaccel.x / 2 > -player->speed.x)player->accel.x = -player->speed.x;
+            else player->accel.x = player->maxaccel.x / 2;
+        }
+        else player->accel.x = 0;
+    }
+    player->movement();
 }
